f19ff78c9eb8615d32ef99b52af6fa2a51edd4dd
kevin lang <langkevina@gmail.com>
2019-06-09 14:08:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  @@emails = []
+  attr_accessor :email
+  def initialize(emails)
+    @email = emails
+    # @@emails << emails
+    @@emails = @email.split /[," "]/
+    @@emails.delete("")
+
+  end
+  def parse
+    @@emails.uniq
+  end
+end
